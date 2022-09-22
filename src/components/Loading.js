eb97ef2f2aa367b4155100d8408d8a172d71219d
Esther Inyang <estherinyang07@gmail.com>
2022-09-22 13:54:38 +0100

    added changes to loading component

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,8 +1,14 @@
 import React from "react";
 
 function Loading() {
+  const styles = {
+    color: "rgb(5, 23, 77)",
+    textAlign: "center",
+    margin: "100px 0",
+  };
+
   return (
-    <div>
+    <div style={styles}>
       <h1>Loading...</h1>
     </div>
   );
